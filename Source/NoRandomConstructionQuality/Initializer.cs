a87474d56c8f7563edcef9830267cc495bcb5833
bwiggins10 <benjimouse2000@gmail.com>
2020-10-31 12:46:56 -0400

    1.2

---
+++ Source/NoRandomConstructionQuality/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace NoRandomConstructionQuality
+{
+    [StaticConstructorOnStartup]
+    public static class Initializer
+    {
+        static Initializer()
+        {
+            MethodInfo m1 = typeof(QualityUtility).GetMethod("GenerateQualityCreatedByPawn", BindingFlags.Static | BindingFlags.Public, null, CallingConventions.Standard, new Type[] { typeof(int), typeof(bool) }, null);
+            MethodInfo m2 = typeof(_QualityUtility).GetMethod("GenerateQualityCreatedByPawn", BindingFlags.Static | BindingFlags.Public);
+
+            if (Detours.TryDetourFromTo(m1, m2))
+            {
+                Log.Message("NoRandomConstructionQuality: QualityUtility.RandomCreationQuality overridden successfully!");
+            }
+
+            MethodInfo m3 = typeof(QualityUtility).GetMethod("SendCraftNotification", BindingFlags.Static | BindingFlags.Public);
+            MethodInfo m4 = typeof(_QualityUtility).GetMethod("_SendCraftNotification", BindingFlags.Static | BindingFlags.Public);
+
+            if (Detours.TryDetourFromTo(m3, m4))
+            {
+                Log.Message("NoRandomConstructionQuality: QualityUtility._SendCraftNotification overridden successfully!");
+            }
+        }
+    }
+}

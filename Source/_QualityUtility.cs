28a0a4d007236b93ae70022f6c8b40f2e07a3eab
Ben <60398078+bwiggins10@users.noreply.github.com>
2021-04-14 16:58:42 -0400

    Skill Cap Patch
    Updated to work with skills that go beyond the Vanilla limit.

--- Source/_QualityUtility.cs
+++ Source/_QualityUtility.cs
@@ -8,45 +8,52 @@ namespace NoRandomConstructionQuality
     {
         public static QualityCategory GetStaticQuality(int relevantSkillLevel)
         {
-            switch (relevantSkillLevel)
+            if (relevantSkillLevel > 20)
             {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    return QualityCategory.Awful;
+                return QualityCategory.Legendary;
+            }
+            else
+            {
+                switch (relevantSkillLevel)
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                        return QualityCategory.Awful;
 
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                    return QualityCategory.Poor;
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        return QualityCategory.Poor;
 
-                case 8:
-                case 9:
-                case 10:
-                    return QualityCategory.Normal;
+                    case 8:
+                    case 9:
+                    case 10:
+                        return QualityCategory.Normal;
 
-                case 11:
-                case 12:
-                case 13:
-                    return QualityCategory.Good;
+                    case 11:
+                    case 12:
+                    case 13:
+                        return QualityCategory.Good;
 
-                case 14:
-                case 15:
-                case 16:
-                    return QualityCategory.Excellent;
+                    case 14:
+                    case 15:
+                    case 16:
+                        return QualityCategory.Excellent;
 
-                case 17:
-                case 18:
-                case 19:
-                    return QualityCategory.Masterwork;
+                    case 17:
+                    case 18:
+                    case 19:
+                        return QualityCategory.Masterwork;
 
-                case 20:
-                    return QualityCategory.Legendary;
+                    case 20:
+                        return QualityCategory.Legendary;
 
-                default:
-                    throw new InvalidOperationException("Unable to determine quality for skill level " + relevantSkillLevel);
+                    default:
+                        throw new InvalidOperationException("Unable to determine quality for skill level " + relevantSkillLevel);
+                }
             }
         }
 

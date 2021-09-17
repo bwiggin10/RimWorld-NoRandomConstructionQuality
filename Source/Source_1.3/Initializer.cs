2eaba474d3ed99aecf2b4d32ecf3f7a9c851fefa
Ben <60398078+bwiggins10@users.noreply.github.com>
2021-09-17 12:23:20 -0400

    Ideology Specialist Role Patch

---
+++ Source/Source_1.3/Initializer.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Reflection;
+using Verse;
+using HarmonyLib;
+
+namespace NoRandomConstructionQuality
+{
+    [StaticConstructorOnStartup]
+    public static class Initializer
+    {
+        static Initializer()
+        {
+            Harmony harmony = new Harmony("NoRandomConstructionQuality_Ben");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            MethodInfo m1 = typeof(QualityUtility).GetMethod("SendCraftNotification", BindingFlags.Static | BindingFlags.Public);
+            MethodInfo m2 = typeof(_QualityUtility).GetMethod("_SendCraftNotification", BindingFlags.Static | BindingFlags.Public);
+
+            if (!Detours.TryDetourFromTo(m1, m2))
+            {
+                Log.Message("NoRandomConstructionQuality: QualityUtility._SendCraftNotification failed to override, please contact mod author");
+            }
+        }
+    }
+}
